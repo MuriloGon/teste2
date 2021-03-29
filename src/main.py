3926f982e35f6992e5f9e3fa0ff3baa42276f0cc
Murilo Gonçalves <murilo.gon@live.com>
2021-03-29 04:01:47 -0300

    novo comit sada sdas as dasd asd
    asd sad
    
    as das da
    as da sd

--- src/main.py
+++ src/main.py
@@ -1,35 +1,57 @@
 import subprocess
 
-def get_branches():
-  proc = subprocess.run(['git branch -a | cat'], shell=True, capture_output=True)
-  rmv_chars = [' ', '*']
-  branches_raw = proc.stdout.decode().split('\n')
 
-  current_branch = ''
-  all_branches = []
-  left_branches = []
+class GitData:
+  def __init__(self):
+    self.__branches__ = self.__fetch_data__()
 
-  for item in branches_raw:
-    branch = item
+  def __fetch_data__(self):
+    proc = subprocess.run(['git branch -a | cat'], shell=True, capture_output=True)
+    rmv_chars = [' ', '*']
+    branches_raw = proc.stdout.decode().split('\n')
 
-    if('*' in item):
-      current_branch=item.replace('*', '').replace(' ','')
+    current_branch = ''
+    all_branches = []
+    left_branches = []
 
-    for char in rmv_chars:
-      branch = branch.replace(char, '')
+    for item in branches_raw:
+      branch = item
 
-    if(item != ''):  
-      all_branches.append(branch)
+      if('*' in item):
+        current_branch=item.replace('*', '').replace(' ','')
+
+      for char in rmv_chars:
+        branch = branch.replace(char, '')
+
+      if(item != ''):  
+        all_branches.append(branch)
+    
+    for branch in all_branches:
+      if(branch != current_branch):
+        left_branches.append(branch)
+
+    return {
+      'current_branch': current_branch,
+      'all_branches': all_branches,
+      'left_branches': left_branches
+      }
+
+  def get_branches(self):
+    return self.__branches__
   
-  for branch in all_branches:
-    if(branch != current_branch):
-      left_branches.append(branch)
+  def get_log(self, branch):
+    output = ''
+    current_branch = self.__branches__['current_branch']
+    subprocess.run(f'git checkout ${branch}', shell=True)
+    
+    a = subprocess.run(f'git log ${branch} | cat', shell=True, capture_output=True)
+    output = a.stdout.decode()
+    subprocess.run(f'git checkout ${current_branch}', shell=True)
+    
+    print(output)
 
-  return {'CurrentBranch': current_branch, 'AllBranches': all_branches, 'LeftBranches': left_branches}
+a = GitData()
 
-a = get_branches()
-
-print('atual', a['CurrentBranch'])
-print('todos', a['AllBranches'])
-print('restantes', a['LeftBranches'])
+print('atual', a.get_branches())
+a.get_log('main')
 
